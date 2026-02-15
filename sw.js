abcd53f19e1bdec01738f9f5a58e708a013caf11
fachrungaming <fachrungaming@gmail.com>
2026-02-15 21:24:20 +0800

    Upload sw.js via automated script

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'imip-detect-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Roboto:wght@300;400;500&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
